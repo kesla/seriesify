2045e371885753dab70c12e05f753463a839eff4
David Björklund <david.bjorklund@gmail.com>
2014-04-30 14:50:23 +0200

    new feature: name result (return object)

--- seriesify.js
+++ seriesify.js
@@ -1,15 +1,48 @@
 var series = require('run-series')
+  , seriesify = function () {
+      var tasks = []
 
-module.exports = function () {
+      return {
+          add: function (fun) {
+            tasks.push(fun)
+            return this
+          }
+        , exec: function (callback) {
+            series(tasks, callback)
+          }
+      }
+    }
+
+seriesify.named = function () {
   var tasks = []
+    , names = []
 
   return {
-      add: function (fun) {
+      add: function (name, fun) {
+        if (!fun) {
+          fun = name
+          name = undefined
+        }
+
+        names.push(name)
         tasks.push(fun)
         return this
       }
     , exec: function (callback) {
-        series(tasks, callback)
+        series(tasks, function (err, array) {
+          if (err) return callback(err)
+
+          var result = {}
+
+          array.forEach(function (data, i) {
+            if (names[i])
+              result[names[i]] = data
+          })
+
+          callback(null, result)
+        })
       }
   }
 }
+
+module.exports = seriesify
